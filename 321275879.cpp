f7ef0056fb135da287890b01f003d60386d95e6c
Luis0004 <luuuismedinaaa23@gmail.com>
2023-09-19 14:24:11 -0500

    primer commit

---
+++ 321275879.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	float base, altura, area;
+	printf("base: ");
+	scanf_s("%f", &base);
+	printf("altura:");
+	scanf_s("%f", &altura);
+	area = ((base * altura) / 2.0);
+	printf("El area es de: %f", area);
+}
